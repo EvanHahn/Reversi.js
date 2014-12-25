a049e44a96d33d580b2c951110d1049746d17dc2
Evan Hahn <me@evanhahn.com>
2014-12-24 23:27:54 -0800

    Generating strings, failing legality

--- test/board.js
+++ test/board.js
@@ -33,4 +33,95 @@ describe('board', () => {
     }
   });
 
+  describe('inBounds', () => {
+
+    let board;
+    beforeEach(() => { board = new Board(); });
+
+    it('correctly detects things in bounds', () => {
+      for (let i = 0; i < 8; i ++) {
+        for (let j = 0; j < 8; j ++) {
+          assert(board.inBounds(i, j));
+        }
+      }
+    });
+
+    it('correctly detects out of bounds', () => {
+      for (let i = -10; i < 0; i ++) {
+        for (let j = 0; j < 8; j ++) {
+          assert(!board.inBounds(i, j));
+        }
+      }
+      for (let i = 8; i < 18; i ++) {
+        for (let j = 0; j < 8; j ++) {
+          assert(!board.inBounds(i, j));
+        }
+      }
+      for (let i = 0; i < 8; i ++) {
+        for (let j = -10; j < 0; j ++) {
+          assert(!board.inBounds(i, j));
+        }
+      }
+      for (let i = 0; i < 8; i ++) {
+        for (let j = 8; j < 18; j ++) {
+          assert(!board.inBounds(i, j));
+        }
+      }
+    });
+
+  });
+
+  describe('checkLegal', () => {
+
+    let board;
+    beforeEach(() => { board = new Board(); });
+
+    it('knows when squares are legal', () => {
+      assert(board.checkLegal(2, 3, black));
+      assert(board.checkLegal(2, 4, white));
+      assert(board.checkLegal(3, 2, black));
+      assert(board.checkLegal(4, 5, black));
+      assert(board.checkLegal(5, 4, black));
+    });
+
+    it('knows occupied squares are illegal', () => {
+      [white, black].forEach(color => {
+        assert(!board.checkLegal(3, 3, color));
+        assert(!board.checkLegal(3, 4, color));
+        assert(!board.checkLegal(4, 3, color));
+        assert(!board.checkLegal(4, 4, color));
+      });
+    });
+
+  });
+
+  describe('toString', () => {
+
+    it('generates an ASCII board', () => {
+      const actual = new Board().toString();
+      const expected = [
+        '    0   1   2   3   4   5   6   7',
+        '   _______________________________',
+        '0 |   |   |   |   |   |   |   |   |',
+        '  |---+---+---+---+---+---+---+---|',
+        '1 |   |   |   |   |   |   |   |   |',
+        '  |---+---+---+---+---+---+---+---|',
+        '2 |   |   |   |   |   |   |   |   |',
+        '  |---+---+---+---+---+---+---+---|',
+        '3 |   |   |   | O | X |   |   |   |',
+        '  |---+---+---+---+---+---+---+---|',
+        '4 |   |   |   | X | O |   |   |   |',
+        '  |---+---+---+---+---+---+---+---|',
+        '5 |   |   |   |   |   |   |   |   |',
+        '  |---+---+---+---+---+---+---+---|',
+        '6 |   |   |   |   |   |   |   |   |',
+        '  |---+---+---+---+---+---+---+---|',
+        '7 |   |   |   |   |   |   |   |   |',
+        '   -------------------------------'
+      ].join('\n');
+      assert.equal(actual, expected);
+    });
+
+  });
+
 });
